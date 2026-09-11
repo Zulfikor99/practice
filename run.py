927342abdf0edd6e7c27229744f0c259b5103c60
Zulfikor Sayfiev <zulfikordev@gmail.com>
2026-09-12 01:53:55 +0900

    feat: python sysytem variables and special methods via dunders

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-message = "Hello World!"
+# Dunder __builtins__,__init__
+message = "Python: Everything is object"
 print(message)
-a = 100
-b = 50
-c = a+b
-print("c:", c)
-print("a:", a)
+
+result = type(message)
+print("result:",result)
+
+""" In Python there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTIONS > print(), len(),input(), type(), str(), int()
+(3) CONSTANTS > True False None
+"""
+
+print(dir(__builtins__))
--- train.js
+++ train.js
@@ -1,3 +1,23 @@
+console.log("==========TASK F==========");
+/*
+Yagona string argumentga ega findDoublers nomli function tuzing
+Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+true yokida false natija qaytarsin.
+*/
+function findDoublers(str) {
+  x = str.split("").sort();
+
+  for (let i = 0; i < str.length; i++) {
+    if (x[i] === x[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(findDoublers("hello"));
+console.log(findDoublers("abcdef"));
+console.log(findDoublers("aastring"));
+
 console.log("==========TASK E==========");
 /*
 
